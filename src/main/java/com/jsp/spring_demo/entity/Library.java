91a662d0872745c4c8b5e6cec3d824cd8fc2719d
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2025-09-13 22:52:32 +0530

    commit 3 : setup project application properties

--- src/main/java/com/jsp/spring_demo/entity/Library.java
+++ src/main/java/com/jsp/spring_demo/entity/Library.java
@@ -1,5 +1,14 @@
 package com.jsp.spring_demo.entity;
 
-public class Library {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Library {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 }
+
