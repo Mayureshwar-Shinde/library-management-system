64237e1a34ba87c0657c6b79af6a1580626db6cd
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2025-09-13 23:22:08 +0530

    commit 4 : implement GET all books

--- src/main/java/com/jsp/spring_demo/controller/BookController.java
+++ src/main/java/com/jsp/spring_demo/controller/BookController.java
@@ -1,20 +1,33 @@
 package com.jsp.spring_demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jsp.spring_demo.dto.BookDTO;
+import com.jsp.spring_demo.responsestructure.ApiResponse;
 import com.jsp.spring_demo.service.BookService;
-
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 	private final BookService bookService;
-	
+
 	@Autowired
 	public BookController(BookService bookService) {
 		this.bookService = bookService;
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<BookDTO>>> getAllBooks() {
+		ApiResponse<List<BookDTO>> apiResponse = new ApiResponse<>(HttpStatus.OK.value(), bookService.findAll(),
+				"Books fetched successfully.");
+		return ResponseEntity.ok(apiResponse);
 	}
 }
 
--- src/main/java/com/jsp/spring_demo/service/BookService.java
+++ src/main/java/com/jsp/spring_demo/service/BookService.java
@@ -1,8 +1,13 @@
 package com.jsp.spring_demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jsp.spring_demo.dto.BookDTO;
+import com.jsp.spring_demo.entity.Book;
 import com.jsp.spring_demo.repository.BookRepository;
 
 
@@ -13,6 +18,27 @@ public class BookService {
 	@Autowired
 	public BookService(BookRepository bookRepository) {
 		this.bookRepository = bookRepository;
+	}
+	
+	public List<BookDTO> findAll() {
+		List<Book> books = bookRepository.findAll();
+		List<BookDTO> bookDTOs = new ArrayList<>();
+		for(Book book : books) {
+			bookDTOs.add(mapToDTO(book));
+		}
+		return bookDTOs;
+	}
+	
+	
+	public BookDTO mapToDTO(Book book) {
+		BookDTO bookDTO = new BookDTO();
+		bookDTO.setId(book.getId());
+		bookDTO.setName(book.getName());
+		bookDTO.setAuthor(book.getAuthor());
+		bookDTO.setDescription(book.getDescription());
+		bookDTO.setLibraryId(book.getLibrary() == null ? null : book.getLibrary().getId());
+		
+		return bookDTO;
 	}
 }
 
