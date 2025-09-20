ebecdddcba7ecdc47ea0b08dc6c3116ced0c63c4
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2025-09-20 10:36:07 +0530

    commit 10 : created book mapper and refactored book service to use that

---
+++ src/main/java/com/jsp/spring_demo/mapper/BookMapper.java
@@ -0,0 +1,37 @@
+package com.jsp.spring_demo.mapper;
+
+
+import org.springframework.stereotype.Component;
+
+import com.jsp.spring_demo.dto.BookDTO;
+import com.jsp.spring_demo.entity.Book;
+import com.jsp.spring_demo.entity.Library;
+
+@Component
+public class BookMapper {
+	
+	public BookDTO toDTO(Book book) {
+		BookDTO bookDTO = new BookDTO();
+		bookDTO.setId(book.getId());
+		bookDTO.setName(book.getName());
+		bookDTO.setAuthor(book.getAuthor());
+		bookDTO.setDescription(book.getDescription());
+		bookDTO.setLibraryId(book.getLibrary() == null ? null : book.getLibrary().getId());
+		return bookDTO;
+	}
+	
+	public Book toEntity(BookDTO bookDTO, Library library) {
+		Book book = new Book();
+		updateEntity(book, bookDTO, library);
+		return book;
+	}
+
+	public Book updateEntity(Book book, BookDTO bookDTO, Library library) {
+		book.setName(bookDTO.getName());
+		book.setAuthor(bookDTO.getAuthor());
+		book.setDescription(bookDTO.getDescription());
+		book.setLibrary(library);
+		return book;
+	}
+}
+
--- src/main/java/com/jsp/spring_demo/service/BookService.java
+++ src/main/java/com/jsp/spring_demo/service/BookService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.jsp.spring_demo.dto.BookDTO;
 import com.jsp.spring_demo.entity.Book;
+import com.jsp.spring_demo.entity.Library;
 import com.jsp.spring_demo.exception.DoesNotExistException;
+import com.jsp.spring_demo.mapper.BookMapper;
 import com.jsp.spring_demo.repository.BookRepository;
 import com.jsp.spring_demo.repository.LibraryRepository;
 
@@ -16,18 +18,22 @@ import com.jsp.spring_demo.repository.LibraryRepository;
 public class BookService {
 	private final BookRepository bookRepository;
 	private final LibraryRepository libraryRepository;
+	private final BookMapper bookMapper;
 
 	@Autowired
-	public BookService(BookRepository bookRepository, LibraryRepository libraryRepository) {
+	public BookService(BookRepository bookRepository,
+			LibraryRepository libraryRepository,
+			BookMapper bookMapper) {
 		this.bookRepository = bookRepository;
 		this.libraryRepository = libraryRepository;
+		this.bookMapper = bookMapper;
 	}
 
 	public List<BookDTO> findAll() {
 		List<Book> books = bookRepository.findAll();
 		List<BookDTO> bookDTOs = new ArrayList<>();
 		for (Book book : books) {
-			bookDTOs.add(mapToDTO(book));
+			bookDTOs.add(bookMapper.toDTO(book));
 		}
 		return bookDTOs;
 	}
@@ -35,44 +41,32 @@ public class BookService {
 	public BookDTO find(int id) {
 		Book book = bookRepository.findById(id)
 				.orElseThrow(() -> new DoesNotExistException("Book with ID: " + id + " does not exist."));
-		return mapToDTO(book);
+		return bookMapper.toDTO(book);
 	}
 
 	public BookDTO createBook(BookDTO bookDTO) {
-		return mapToDTO(bookRepository.save(mapToEntity(new Book(), bookDTO)));
+		Integer libraryId = bookDTO.getLibraryId();
+		Library library = libraryId == null ? null
+				: libraryRepository.findById(libraryId).orElseThrow(
+						() -> new DoesNotExistException("Library with ID: " + libraryId + " does not exist."));
+		Book book = bookRepository.save(bookMapper.toEntity(bookDTO, library));
+		return bookMapper.toDTO(book);
 	}
 	
 	public BookDTO updateBook(BookDTO bookDTO, int id) {
 		Book book = bookRepository.findById(id).orElseThrow(
 				() -> new DoesNotExistException("Book with ID: " + id + " does not exist."));
-		return mapToDTO(bookRepository.save(mapToEntity(book, bookDTO)));
+		Integer libraryId = bookDTO.getLibraryId();
+		Library library = libraryId == null ? null
+				: libraryRepository.findById(libraryId).orElseThrow(
+						() -> new DoesNotExistException("Library with ID: " + libraryId + " does not exist."));
+		Book savedBook = bookRepository.save(bookMapper.updateEntity(book, bookDTO, library));
+		return bookMapper.toDTO(savedBook);
 	}
 	
 	public void deleteBook(int id) {
 		Book book = bookRepository.findById(id).orElseThrow(
 				() -> new DoesNotExistException("Book with ID: " + id + " does not exist."));
 		bookRepository.delete(book);
-	}
-	
-	
-	public BookDTO mapToDTO(Book book) {
-		BookDTO bookDTO = new BookDTO();
-		bookDTO.setId(book.getId());
-		bookDTO.setName(book.getName());
-		bookDTO.setAuthor(book.getAuthor());
-		bookDTO.setDescription(book.getDescription());
-		bookDTO.setLibraryId(book.getLibrary() == null ? null : book.getLibrary().getId());
-		return bookDTO;
-	}
-
-	public Book mapToEntity(Book book, BookDTO bookDTO) {
-		book.setName(bookDTO.getName());
-		book.setAuthor(bookDTO.getAuthor());
-		book.setDescription(bookDTO.getDescription());
-		Integer libraryId = bookDTO.getLibraryId();
-		book.setLibrary(libraryId == null ? null
-				: libraryRepository.findById(libraryId).orElseThrow(
-						() -> new DoesNotExistException("Library with ID: " + libraryId + " does not exist.")));
-		return book;
 	}
 }
